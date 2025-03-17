7b596770863bbd16640f395059689bf558d8ace6
Chiranjeevi sai charan <saicharan922014@gmail.com>
2025-03-17 21:37:55 +0530

    added changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow frontend access
+    origin: process.env.FRONTEND_URL, // Allow frontend access
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
