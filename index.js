b2abc253d2d2db55ad498798b9efadc2f37969ca
Chiranjeevi sai charan <saicharan922014@gmail.com>
2025-03-17 21:41:34 +0530

    added changes

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // Allow frontend access
+    origin:
+      "https://frontend-react-9m76rc98b-saicharan2014s-projects.vercel.app", // Allow frontend access
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
